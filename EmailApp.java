cbf09b5fa0f84d543cf329d7b6709f91b91235ca
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-01-14 03:24:36 +0000

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,20 @@
+package emailapp;
+
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email em1=new Email("John","Smith");
+
+       // em1.setAltEmail("");
+       // em1.getAlternateEmail();
+
+       // em1.changePasssword("");
+       // em1.getPassword();
+
+       // em1.setMailBoxCapacity();
+       // em1.getMailBoxCapacity();
+
+       // em1.showInfo();
+        
+    }
+}
